c93e03b21d8df8ac05d0175b2ad8ead0840c62d4
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2021-08-21 10:55:45 -0500

    Dark theme as default

--- styles/system.ts
+++ styles/system.ts
@@ -1,8 +1,25 @@
 export const monos = {
-  mc000: '#FFF',
-  mc900: '#333',
+  mc000: '#f0f6fc',
+  mc100: '#c9d1d9',
+  mc200: '#b1bac4',
+  mc300: '#8b949e',
+  mc400: '#6e7681',
+  mc500: '#484f58',
+  mc600: '#30363d',
+  mc700: '#21262d',
+  mc800: '#161b22',
+  mc900: '#0d1117',
 }
 
 export const colors = {
-  blue500: 'blue',
+  blue000: '#cae8ff',
+  blue100: '#a5d6ff',
+  blue200: '#79c0ff',
+  blue300: '#58a6ff',
+  blue400: '#388bfd',
+  blue500: '#1f6feb',
+  blue600: '#1158c7',
+  blue700: '#0d419d',
+  blue800: '#0c2d6b',
+  blue900: '#051d4d',
 }
--- styles/themes/defaultTheme.ts
+++ styles/themes/defaultTheme.ts
@@ -1,8 +1,8 @@
 import * as sys from 'styles/system'
 
 export const defaultTheme = {
-  fontColor: sys.monos.mc900,
-  fontColor__Inverse: sys.monos.mc000,
-  bgColor: sys.monos.mc000,
-  linkColor: sys.colors.blue500,
+  fontColor: sys.monos.mc100,
+  fontColor__Inverse: sys.monos.mc900,
+  bgColor: sys.monos.mc900,
+  linkColor: sys.colors.blue300,
 }
