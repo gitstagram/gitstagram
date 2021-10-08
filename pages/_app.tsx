cfe335365435ef47541677f02bebc02c01f1609c
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2021-10-08 15:10:33 -0500

    feat: navigation progress topbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 import type { Session } from 'next-auth'
 import { Provider } from 'next-auth/client'
@@ -21,6 +22,8 @@ import 'react-toastify/dist/ReactToastify.css'
 import { DefaultLayout } from 'components/layouts'
 import { LoadingContextProvider } from 'components/contexts/loading'
 
+import TopBarProgress from 'react-topbar-progress-indicator'
+
 // Calculates proper 100vh size without iOS Safari navigation and document bars
 // https://dev.to/maciejtrzcinski/100vh-problem-with-ios-safari-3ge9
 const initializeAppHeight = `
@@ -40,6 +43,22 @@ interface PageProps {
 }
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  const router = useRouter()
+  const [progress, setProgress] = useState(false)
+
+  useEffect(() => {
+    const handleRouteChange = () => setProgress(true)
+    const handleRouteComplete = () => setProgress(false)
+
+    router.events.on('routeChangeStart', handleRouteChange)
+    router.events.on('routeChangeComplete', handleRouteComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+      router.events.off('routeChangeComplete', handleRouteComplete)
+    }
+  }, [router.events])
+
   return (
     <LoadingContextProvider>
       <Provider session={(pageProps as PageProps).session}>
@@ -57,6 +76,7 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
           </Head>{' '}
           <ApolloProvider client={apolloClient}>
             <DefaultLayout>
+              {progress && <TopBarProgress />}
               <Component {...pageProps} />
             </DefaultLayout>
           </ApolloProvider>
