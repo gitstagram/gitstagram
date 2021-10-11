78612a58de887f2e3a4b320bff079516c01334b9
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2021-10-11 10:17:44 -0500

    ci: test repo transfer deployment

--- components/feed.tsx
+++ components/feed.tsx
@@ -3,5 +3,5 @@ import { useGetViewerGitstagramLibraryQuery } from 'graphql/generated'
 
 export const Feed = (): JSX.Element => {
   const { data, loading } = useGetViewerGitstagramLibraryQuery()
-  return loading ? <>Loading</> : <>{JSON.stringify(data)}</>
+  return loading ? <>Loading</> : <>Data: {JSON.stringify(data)}</>
 }
