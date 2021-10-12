6cbacace08ab9c638e69456c28fa840c678ade31
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2021-10-11 23:52:21 -0500

    refactor: consolidate metadata struct functions into one file

--- helpers/validation/isMetadata.ts
+++
@@ -1,6 +0,0 @@
-import { is } from 'superstruct'
-import { MetadataStruct, Metadata } from 'helpers/validation/structs/metadata'
-
-export const isMetadata = (value: unknown): value is Metadata => {
-  return is(value, MetadataStruct)
-}
---
+++ helpers/validation/validateMetadata.ts
@@ -0,0 +1,49 @@
+import { create, mask, is } from 'superstruct'
+import * as structs from 'helpers/validation/structs/metadata'
+import { isAnyRecord } from 'helpers/validation/isAnyRecord'
+import { stringCombinatorials } from 'helpers/string/stringCombinatorials'
+
+export const coerceMetaLoginSearchParts = (
+  anyValue: unknown
+): structs.MetaLoginSearchParts => {
+  const validMetaLoginSearchParts = create(
+    anyValue,
+    structs.MetaLoginSearchPartsStruct
+  )
+  return validMetaLoginSearchParts
+}
+
+export const coerceMetadata = (anyValue: unknown): structs.Metadata => {
+  // If value is not a record, make it an empty record
+  const metadataRecord = isAnyRecord(anyValue) ? anyValue : {}
+
+  // use mask to first ignore any extraneous keys
+  // create value using struct coercion logic
+  const validMetadata = create(
+    mask(metadataRecord, structs.MetadataStruct),
+    structs.MetadataStruct
+  )
+  return validMetadata
+}
+
+export const isMetadata = (value: unknown): value is structs.Metadata => {
+  return is(value, structs.MetadataStruct)
+}
+
+/*
+ * Metadata is stored in `gitstagram-library` repository description
+ *   - Description field characters max out at 65536
+ *   - Provides additional data to help with gitstagram discoverability, etc
+ */
+export const getMetadataJson = (login: string): string => {
+  /*
+   * Breaks up login name into constituent parts
+   *   - "foobar" => "foo", "foob", "fooba", "foobar"
+   *   - It is usually not possible to search a partial login name for a repo
+   *   - This allows Github to find `gitstagram-library`'s with login name partials
+   */
+  const loginSearchParts = stringCombinatorials(login, 3)
+  const metadata = coerceMetadata({ loginSearchParts })
+  const jsonStr = JSON.stringify(metadata)
+  return jsonStr
+}
